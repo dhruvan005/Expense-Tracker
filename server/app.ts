6adbd9aaafe232f542abd2280ac50b213c7f3c36
dhruvan005 <pateldhruvan52@gmail.com>
2025-02-02 22:52:17 +0530

    update server module path

--- server/app.ts
+++ server/app.ts
@@ -2,18 +2,17 @@ import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 import { expensesRoute } from './routes/expenses'
 import { cors } from 'hono/cors'
-import { serveStatic } from 'hono/deno'
+import { serveStatic } from 'hono/bun'
 const app = new Hono()
 
 app.use(logger())
 app.use('/api/*', cors())
-app.get('/', (c) => c.text('Hello Bun! hi'))
 
-app.use('/static/*', serveStatic({ root: './' }))
-app.use('/favicon.ico', serveStatic({ path: './favicon.ico' }))
-app.get('/', (c) => c.text('You can access: /static/hello.txt'))
-app.get('*', serveStatic({ path: './static/fallback.txt' }))
+
+//  for always running the client app 
 
 app.route("api/expenses", expensesRoute)
+app.use('*', serveStatic({ root: './client/dist' }))
+app.get('*', serveStatic({ path: './client/dist/index.html' }))
 
 export default app
